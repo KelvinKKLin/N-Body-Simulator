e00f08b231dd4499a703c71033ab70e5de2ff160
Kelvin Lin <kelvinkklin@gmail.com>
2018-04-17 18:17:50 -0400

    Fixed layover problem

--- properties.h
+++ properties.h
@@ -4,26 +4,25 @@
 #include "vector3d.h"
 
 //light particles are the fastest
-double velocityLightMin = -3000;
-double velocityLightMax = 3000;
+double velocityLightMin = -90;
+double velocityLightMax = 90;
 
-double velocityMediumMin = -10000;
-double velocityMediumMax = 10000;
+double velocityMediumMin = -60;
+double velocityMediumMax = 60;
 
 //heavy particles are the slowest
-double velocityHeavyMin = -30000;
-double velocityHeavyMax = 30000;
+double velocityHeavyMin = -30;
+double velocityHeavyMax = 30;
 
 //mass
-double massLightMin  = 0.5e30;
-double massLightMax  = 1e30;
+double massLightMin  = 1;
+double massLightMax  = 5;
 
-double massMediumMin = 1e30;
-double massMediumMax = 1.5e30;
+double massMediumMin = 6;
+double massMediumMax = 10;
 
-double massHeavyMin  = 1.5e30;
-double massHeavyMax  = 2e30;
-
+double massHeavyMin  = 11;
+double massHeavyMax  = 15;
 
 //colours
 vec3 colourLight = vec3(0,0,1);
